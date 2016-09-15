4508182944bd01f53867765a7cab66aae33f4803
Nikolay Yakimov <root@livid.pp.ru>
2016-09-15 22:43:13 +0300

    Rather read lifx token on start

--- xmonad.hs
+++ xmonad.hs
@@ -122,7 +122,7 @@ main = xmonad $ do
   -- M = M4 = RALT
   -- M1 = LALT
   -- M3 = RCTL
-  lifxTokenVar <- io $ newIORef $ Left ""
+  lifxToken <- io $ try (read <$> readFile ".lifxToken")
   let
     layout' f = withWindowSet $ f . description . W.layout . W.workspace . W.current
     lifxPower :: Lifx.PowerState -> X ()
@@ -130,21 +130,10 @@ main = xmonad $ do
     lifxBrightness :: Float -> X ()
     lifxBrightness v = lifxCommand $ Lifx.group "Room" . Lifx.brightness v
     lifxCommand :: Lifx.Lifx a => (Lifx.LifxCommand a -> Lifx.LifxCommand a) -> X ()
-    lifxCommand c = io Prelude.$
-      lifxToken >>=
-        \case
-          Left err -> hPutStrLn stderr err
-          Right tok' ->
-            void . forkIO . void $ Lifx.command tok' c
-    lifxToken :: IO (Either String String)
-    lifxToken =
-      readIORef lifxTokenVar >>=
-        \case
-          Left _ -> do
-            res <- (left (show :: SomeException -> String) . right read) <$> try (readFile ".lifxToken")
-            writeIORef lifxTokenVar res
-            return res
-          x -> return x
+    lifxCommand c
+      | Right tok <- lifxToken = io $ void . forkIO . void $ Lifx.command tok c
+      | Left err <- lifxToken = io $ hPrint stderr (err :: SomeException)
+
 
     myXPConfig :: XPConfig
     myXPConfig = amberXPConfig{ Pr.font="xft:Fira Mono:pixelsize=24"
